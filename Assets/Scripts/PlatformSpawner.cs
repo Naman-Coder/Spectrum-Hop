df9f3b37113c5627edf21a8a4cacaaec33333293
Naman-Coder <naman.pandey101@gmail.com>
2023-08-16 20:04:01 +0530

    Created a platform spawner script

---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] platformPrefabs;
+    private Vector3 spawnPoint = new (0f, 0f, 1.5f);          
+    private float spawnInterval = 0.5f;       
+    private float timer = 0f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            SpawnPlatform();
+            spawnPoint += new Vector3(0f, 0f, 1.5f);
+            timer = 0f; 
+        }
+    }
+
+    private void SpawnPlatform()
+    {
+        int randomIndex = Random.Range(0, platformPrefabs.Length);
+        GameObject platformPrefab = platformPrefabs[randomIndex];
+
+        Instantiate(platformPrefab, spawnPoint, Quaternion.identity, transform);
+    }
+}
