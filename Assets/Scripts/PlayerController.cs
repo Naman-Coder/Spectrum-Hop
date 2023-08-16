93c4d05c0e82f14a41613fe83cd89708102bdd1f
Naman-Coder <naman.pandey101@gmail.com>
2023-08-16 21:54:33 +0530

    Added jump mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
-    private float speed = 1f;
+    private float speed = 1.25f;
+    private float jumpForce = 5f; 
+    private float raycastDistance = 1f;
+    private bool isGrounded;
+    private bool pressedEnter;
 
     private void FixedUpdate() 
     {
-        Vector3 forwardMovement = transform.forward * speed * Time.deltaTime;
-        rb.MovePosition(rb.position + forwardMovement);
+        ForwardMovement();
+        GroundCheck();
     }
 
+    private void Update() {
+        Jump();
+    }
+
+    private void ForwardMovement() 
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+            pressedEnter = true;
+        if(pressedEnter) {
+            Vector3 forwardMovement = transform.forward * speed * Time.deltaTime;
+            rb.MovePosition(rb.position + forwardMovement);
+        }
+    }
     
+    private void GroundCheck()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, raycastDistance);
+    }
+
+    private void Jump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
 }
+
+
+
+
+
+
+
