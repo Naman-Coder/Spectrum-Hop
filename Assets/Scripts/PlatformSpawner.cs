7cd91a09b4c12be7f1a5be32d3f9b504634544a8
Naman-Coder <naman.pandey101@gmail.com>
2023-08-18 22:52:08 +0530

    spawn platform based on an algorithm

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -5,31 +5,48 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] platformPrefabs;
-    private Vector3 spawnPoint = new (0f, 0f, 1f);          
+    [SerializeField] private ColorCheck colorCheck;
+
+    private bool isCorrectPlatformSpawned;
+    private Vector3 spawnPoint = new (0f, 0f, 1.5f);          
     private float spawnInterval = 0.5f;       
     private float timer = 0f;
 
     private void Update()
     {
         timer += Time.deltaTime;
-
         if (timer >= spawnInterval)
         {
             SpawnPlatform();
-            spawnPoint += new Vector3(0f, 0f, 1f);
+            spawnPoint += new Vector3(0f, 0f, 1.5f);
             timer = 0f; 
         }
     }
 
     private void SpawnPlatform()
     {
-        int randomIndex = Random.Range(0, platformPrefabs.Length);
-        GameObject platformPrefab = platformPrefabs[randomIndex];
+        //Spawns platforms alternatively
+        if(!isCorrectPlatformSpawned)
+        {
+            Instantiate(platformPrefabs[colorCheck.currentColorIndex], spawnPoint, Quaternion.identity, transform);
+            isCorrectPlatformSpawned = true;
+        }
+        else
+        {
+            Instantiate(platformPrefabs[RandomExcept(0, platformPrefabs.Length, colorCheck.currentColorIndex)], spawnPoint, Quaternion.identity, transform);
+            isCorrectPlatformSpawned = false;
+        }
 
-        Instantiate(platformPrefab, spawnPoint, Quaternion.identity, transform);
-        foreach (Transform child in transform)
-         {
+        foreach (Transform child in transform) // Changes instantiated objects' layer
+        {
             child.gameObject.layer = 6;
-         }
+        }
+    }
+
+    public int RandomExcept(int min, int max, int except)
+    {
+        int random = Random.Range(min, max);
+        if (random == except) random = (random + 1) % max;
+        return random;
     }
 }
