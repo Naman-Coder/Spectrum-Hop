becc0413c7f5220302e8da2f2f8eecffb67c242b
Naman-Coder <naman.pandey101@gmail.com>
2023-08-18 22:52:58 +0530

    add ability to double jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,40 +7,60 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
+
+    private int maxJumps = 1; // Max jumps excluding initial jump
+    private int remainingJumps;
     private float speed = 1.25f;
     private float jumpForce = 5f; 
-    private float raycastDistance = 0.5f;
     private bool isGrounded;
+    private float distToGround;
+
+    void Start ()
+    {
+        remainingJumps = maxJumps;
+        distToGround = GetComponent<Collider>().bounds.extents.y;
+    }
 
     private void FixedUpdate() 
     {
         ForwardMovement();
-        GroundCheck();
     }
 
     private void Update() {
         Jump();
+        GroundCheck();
     }
 
     private void ForwardMovement() 
     {
         Vector3 forwardMovement = transform.forward * speed * Time.deltaTime;
         rb.MovePosition(rb.position + forwardMovement);
-        
     }
     
     private void GroundCheck()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, raycastDistance);
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f);
+        if (isGrounded)
+        {
+            remainingJumps = maxJumps;
+        }
     }
 
     private void Jump()
     {
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        if ((isGrounded || remainingJumps > 0) && Input.GetButtonDown("Jump"))
         {
+            Debug.Log(isGrounded);
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z); // Reset vertical velocity
+            
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
+
+            if (!isGrounded)
+            {
+                remainingJumps--;
+            }
     }
+}
 }
 
 
